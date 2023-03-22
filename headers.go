01bcc619ea2342bc6208c8490a4e95be1943bd0f
David Lee <lihongyi0512@gmail.com>
2023-03-22 19:57:31 +0800

    “chirpy”

---
+++ headers.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func main() {
 	// Add a handler for the root path
 	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
+	// Add a handler for the /healthz path
+	mux.HandleFunc("/healthz", healthzHandler)
+
 	// Wrap the mux in a custom middleware function that adds CORS headers to the response
 	corsMux := middlewareCors(mux)
 
@@ -25,20 +28,19 @@ func main() {
 
 	// Use the server's ListenAndServe method to start the server
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	// log.Fatal(srv.ListenAndServe())
 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		panic(err)
 	}
 }
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	// Write the Content-Type header
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	// Write the status code using w.WriteHeader
+	w.WriteHeader(http.StatusOK)
+
+	// Write the body text using w.Write
+	w.Write([]byte("OK"))
 }
