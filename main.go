06e487638cc21577cb10ffdb39e0208f1c358775
David Lee <lihongyi0512@gmail.com>
2023-03-22 15:44:45 +0800

    “chirpy”

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	// Create a new http.ServeMux
+	mux := http.NewServeMux()
+
+	// Wrap the mux in a custom middleware function that adds CORS headers to the response
+	corsMux := middlewareCors(mux)
+
+	// Create a new http.Server and use the corsMux as the handler
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+	// Use the server's ListenAndServe method to start the server
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		panic(err)
+	}
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
