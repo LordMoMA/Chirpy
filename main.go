49ad45c8e8cb3bc234fb7e8d3e699baef1bb9def
David Lee <lihongyi0512@gmail.com>
2023-03-23 04:20:49 +0800

    “chirpy”

--- main.go
+++ main.go
@@ -77,3 +77,16 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	// Write the body text using w.Write
 	w.Write([]byte("OK"))
 }
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
