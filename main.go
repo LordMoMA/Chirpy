d705db3e58573ffc3107d3d2863a424efa207a3d
David Lee <lihongyi0512@gmail.com>
2023-03-23 04:11:15 +0800

    “chirpy”

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"sync/atomic"
 )
 
-apiCfg := apiConfig{
+type apiConfig struct {
 	fileserverHits uint64
 }
 
@@ -15,22 +15,19 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-
 	// Create a new apiConfig struct to hold the request count
-	apiCfg := apiConfig{}
+	apiCfg := &apiConfig{}
 	// Create a new http.ServeMux
 	mux := http.NewServeMux()
 
 	// Wrap the http.FileServer handler with a custom middleware function that increments the request count
 	mux.Handle("/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot))))
 
-
 	// Add a handler for the /healthz path
 	mux.HandleFunc("/healthz", healthzHandler)
 
 	// Add a handler for the /metrics path that returns the request count as plain text
 	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
-
 
 	// Wrap the mux in a custom middleware function that adds CORS headers to the response
 	corsMux := middlewareCors(mux)
@@ -49,6 +46,27 @@ func main() {
 	}
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Increment the request count
+		atomic.AddUint64(&cfg.fileserverHits, 1)
+
+		// Call the next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	// Write the Content-Type header
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	// Write the status code using w.WriteHeader
+	w.WriteHeader(http.StatusOK)
+
+	// Write the body text using w.Write
+	fmt.Fprintf(w, "Hits: %d", atomic.LoadUint64(&cfg.fileserverHits))
+}
+
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	// Write the Content-Type header
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -58,8 +76,4 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Write the body text using w.Write
 	w.Write([]byte("OK"))
-}
-
-func metricsHandler(w http.ResponseWriter, r *http.Request){
-
 }
