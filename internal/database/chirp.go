2ef52f504f7edf59b19e1bed54bb3efdc2e57df8
David Lee <lihongyi0512@gmail.com>
2023-03-28 23:06:44 +0800

    “clean-architecture”

---
+++ internal/database/chirp.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"sort"
+)
+
+// CreateChirp creates a new chirp and saves it to disk
+func (db *DB) CreateChirp(body string) (Chirp, error) {
+
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	id := len(dbStructure.Chirps) + 1
+	chirp := Chirp{
+		ID:   id,
+		Body: body,
+	}
+
+	dbStructure.Chirps[id] = chirp
+	if err := db.writeDB(dbStructure); err != nil {
+		return Chirp{}, err
+	}
+	return chirp, nil
+}
+
+// GetChirps returns all chirps in the database
+func (db *DB) GetChirps() ([]Chirp, error) {
+
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+
+	chirps := make([]Chirp, 0, len(dbStructure.Chirps))
+	for _, chirp := range dbStructure.Chirps {
+		chirps = append(chirps, chirp)
+	}
+
+	sort.Slice(chirps, func(i, j int) bool {
+		return chirps[i].ID < chirps[j].ID
+	})
+
+	return chirps, nil
+}
--- internal/database/db.go
+++ internal/database/db.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"sort"
 	"sync"
 )
 
@@ -45,47 +44,6 @@ func NewDB(path string) (*DB, error) {
 	}
 	fmt.Println("calling NewDB()... 📚")
 	return db, nil
-}
-
-// CreateChirp creates a new chirp and saves it to disk
-func (db *DB) CreateChirp(body string) (Chirp, error) {
-
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return Chirp{}, err
-	}
-
-	id := len(dbStructure.Chirps) + 1
-	chirp := Chirp{
-		ID:   id,
-		Body: body,
-	}
-
-	dbStructure.Chirps[id] = chirp
-	if err := db.writeDB(dbStructure); err != nil {
-		return Chirp{}, err
-	}
-	return chirp, nil
-}
-
-// GetChirps returns all chirps in the database
-func (db *DB) GetChirps() ([]Chirp, error) {
-
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return nil, err
-	}
-
-	chirps := make([]Chirp, 0, len(dbStructure.Chirps))
-	for _, chirp := range dbStructure.Chirps {
-		chirps = append(chirps, chirp)
-	}
-
-	sort.Slice(chirps, func(i, j int) bool {
-		return chirps[i].ID < chirps[j].ID
-	})
-
-	return chirps, nil
 }
 
 // ensureDB creates a new database file if it doesn't exist
